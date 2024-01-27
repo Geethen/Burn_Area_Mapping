2db55305635fe4b6e73d4ad05bdbf39b73c6df34
Geethen <geethen.singh@gmail.com>
2024-01-27 15:11:17 +0200

    added data_preprocessing for Sentinel-2 and Landsat 4-9

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptoools import setup, find_packages
+from setuptools import setup, find_packages
 from typing import List
 def get_requirements(file_path:str) ->List[str]:
     """
--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -16,12 +16,5 @@ class customException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-if __name__== "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise customException(e, sys)
 
 
