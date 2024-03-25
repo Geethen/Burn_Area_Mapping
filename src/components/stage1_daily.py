8ad06be681bb92a7d2d5f1d9b0d3ba8191b55927
Geethen <geethen.singh@gmail.com>
2024-03-25 20:23:39 +0200

    added chdir to inference

--- src/components/stage1_daily.py
+++ src/components/stage1_daily.py
@@ -1,10 +1,12 @@
 import ee
+import os
 from inference import Inference
 
 service_account = 'bam-981@ee-geethensingh.iam.gserviceaccount.com'
 credentials = ee.ServiceAccountCredentials(service_account, 'secret.json')
 ee.Initialize(credentials)
 
+os.chdir(r".\src\components")
 inference_pipeline = Inference()
 sceneList = inference_pipeline.initiate_inference_pipeline('LANDSAT_8', 'South Africa')
 sceneList
