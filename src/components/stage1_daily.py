dc262e56b55a02af1af63a8348242bcf3eb21677
Geethen <geethen.singh@gmail.com>
2024-03-25 20:41:59 +0200

    chdir v4

--- src/components/stage1_daily.py
+++ src/components/stage1_daily.py
@@ -6,8 +6,17 @@ service_account = 'bam-981@ee-geethensingh.iam.gserviceaccount.com'
 credentials = ee.ServiceAccountCredentials(service_account, 'secret.json')
 ee.Initialize(credentials)
 
-print(os.getcwd())
-os.chdir(r"\home\runner\work\Burn_Area_Mapping\Burn_Area_Mapping\src\components")
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Define the relative path to the target directory
+relative_path = "src/components"
+
+# Join the current directory with the relative path to get the absolute path
+target_directory = os.path.join(current_directory, relative_path)
+
+# Change to the target directory
+os.chdir(target_directory)
 inference_pipeline = Inference()
 sceneList = inference_pipeline.initiate_inference_pipeline('LANDSAT_8', 'South Africa')
 sceneList
