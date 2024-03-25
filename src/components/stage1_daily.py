36fa612101d095e89154e9a522da893c2b4c5e39
Geethen <geethen.singh@gmail.com>
2024-03-25 20:37:35 +0200

    chdir v3

--- src/components/stage1_daily.py
+++ src/components/stage1_daily.py
@@ -7,7 +7,7 @@ credentials = ee.ServiceAccountCredentials(service_account, 'secret.json')
 ee.Initialize(credentials)
 
 print(os.getcwd())
-os.chdir(r"src\components")
+os.chdir(r"\home\runner\work\Burn_Area_Mapping\Burn_Area_Mapping\src\components")
 inference_pipeline = Inference()
 sceneList = inference_pipeline.initiate_inference_pipeline('LANDSAT_8', 'South Africa')
 sceneList
