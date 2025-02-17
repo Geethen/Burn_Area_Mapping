a4cf731cd281e7468bb833f220d3918143757e96
Geethen <geethen.singh@gmail.com>
2025-02-17 15:49:13 +0000

    to GH runner for WC test

--- src/components/stage1_daily.py
+++ src/components/stage1_daily.py
@@ -10,6 +10,7 @@ credentials = ee.ServiceAccountCredentials(service_account, 'secret.json')
 ee.Initialize(credentials)
 
 inference_pipeline = Inference()
+# Western Cape - fynbos and renosterveld
 ecoregions = ee.FeatureCollection("RESOLVE/ECOREGIONS/2017")
 aoi = ee.FeatureCollection(ecoregions.filter(ee.Filter.inList('ECO_ID', [89,90]))).bounds()
 sceneList = inference_pipeline.initiate_inference_pipeline('LANDSAT_8', aoi, '2016-01-01', '2017-01-01')
